52420e4550031232a021185179c35d44c27f627e
TerrorScript <delangenthomas@gmail.com>
2023-12-11 22:10:41 +0100

    Experimented with nuitika build tool. It added files main.cmd, main.exe and the directory main.build

---
+++ main.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=C:\Users\ADMINI~1\AppData\Local\Programs\Python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\Administrator\AppData\Local\Programs\Python\Python310
+"%~dp0main.exe" %*
